f91a3b78e107f07373fac3fca858d1183eb2cfc3
EC2 Default User <erika.yyma@gmail.com>
2019-12-17 07:56:27 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   #GET /users/new
@@ -56,15 +57,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(
         :name, :email, :password,
         :password_confirmation)
-    end
-    
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-        unless logged_in?
-        store_location
-         flash[:danger] = "Please log in."
-          redirect_to login_url
-        end
     end
     
     # 正しいユーザーかどうか確認
